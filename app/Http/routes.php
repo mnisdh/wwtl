c7c161b1075b5ca646463ae09246d4081ba17c92
aries2058 <aries2058@gmail.com>
2017-02-02 21:37:46 +0900

    ip addr locgic update

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,7 +16,7 @@ Route::auth();
 
 Route::get('/', 'HomeController@index');
 Route::get('/home', 'HomeController@index');
-Route::get('/home_', 'HomeController@index');
+Route::get('/home_', 'HomeController@indexNew');
 
 Route::post('passwordmail', 'Auth\PasswordController@sendMail');
 Route::post('authmail', 'Auth\AuthController@sendMail');
